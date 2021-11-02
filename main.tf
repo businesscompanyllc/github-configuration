24c85eb7d9441a5468bc542abd2d3a59d9356dbd
Kyle McCormick <kmccormick@edx.org>
2021-11-02 12:10:47 -0400

    feat: add alice to org as organization manager (#2)

--- main.tf
+++ main.tf
@@ -37,55 +37,9 @@ variable "github_configuration_bot_username" {
   default     = "businesscompanyllc-gh-config-bot"
 }
 
-variable "test_username_0" {
-  type        = string
-  description = "The owner username."
-}
-
-variable "test_username_1" {
-  type        = string
-  description = "A username to test with."
-}
-
-variable "test_username_2" {
-  type        = string
-  description = "A second username to test with."
-}
-
 # Use the GitHub-Terraform provider.
 # The following two environment variables must be provided:
 #  * GITHUB_TOKEN: personal access token of GH account used to apply this.
 #  * GITHUB_OWNER: user or organization to create resources under.
 provider "github" {
-}
-
-# Let the org management team push to this repo.
-#resource "github_team_repository" "organization_managers_write_to_gh_config" {
-#  team_id    = github_team.organization_managers.id
-#  repository = github_repository.github_configuration.name
-#  permission = "push"
-#}
-
-# Add user 0 to our GH organization as owner.
-resource "github_membership" "user_0" {
-  username = var.test_username_0
-  role     = "admin"
-}
-
-# Add user 1 to our GH organization as member.
-resource "github_membership" "user_1" {
-  username = var.test_username_1
-  role     = "member"
-}
-
-resource "github_team" "organization_managers" {
-  name        = "organization-managers"
-  description = "People who can manager our GH org by merging to github-configuration."
-  privacy     = "closed"
-}
-
-resource "github_team_membership" "membership_for_user_1" {
-  team_id  = github_team.organization_managers.id
-  username = github_membership.user_1.username
-  role     = "member"
 }
